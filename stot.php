37eb59a4a24942b08d44edac8f90e2b9bc09617a
Julie <julie@isager.dk>
2023-06-03 18:18:48 +0200

    ny side stot

---
+++ stot.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="da">
+<head>
+    <meta charset="utf-8">
+
+    <title>Støt</title>
+
+    <meta name="robots" content="All">
+    <meta name="author" content="Udgiver">
+    <meta name="copyright" content="Information om copyright">
+
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+    <script src="https://kit.fontawesome.com/4c1c4ae382.js" crossorigin="anonymous"></script>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+
+<div class="container-fluid bg-gron">
+    <div class="row">
+        <div class="col">
+            image
+        </div>
+    </div>
+</div>
+
+
+<div class="container-fluid bg-white">
+    <div class="row">
+        <div class="col text-center">
+            Vi mangler Veteranhave venner! Er det dig?
+        </div>
+    </div>
+</div>
+
+
+<div class="container-fluid bg-baggrund">
+    <div class="row">
+        <div class="col text-center">
+
+            Du kan hjælpe ved arrangementer og aktiviteter i haven
+            <br>
+            Vær med til at købe ind, stå for salg af kaffe og kage og ellers hjælpe, hvor der må være brugfor det.
+            <br>
+            Hjælpe med at uddele vores julehjælp til veteranfamilier
+            <br>
+            Pakke og bringe ud til de glade modtagere.
+            <br>
+            Vise besøgende rundt i haven
+            <br>
+            Svare på spørgsmål og fortælle om VeteranHaven som have og forening.
+            <br>
+            Deltage i arrangementer, der skaber opmærksomhed om VeteranHaven
+            <br>
+            Og vigtigst af alt.
+            <br>
+            Bidrage med dit humør og gåpåmod.
+
+        </div>
+    </div>
+</div>
+
+<div class="container-fluid bg-light">
+    <div class="row">
+        <div class="col text-center">
+            Sådan kan du støtte os
+        </div>
+    </div>
+</div>
+
+<div class="container-fluid bg-light">
+    <div class="row">
+        <div class="col-12 col-md-6">
+
+            <div class="card" style="width: 18rem;">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="col-12 col-md-6">
+
+            <div class="card" style="width: 18rem;">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+</div>
+
+
+<div class="container-fluid bg-white">
+    <div class="row">
+        <div class="col text-center">
+            Sponsoraftaler 2023
+        </div>
+    </div>
+</div>
+
+<div class="container-fluid bg-white">
+    <div class="row">
+        <div class="col">
+
+        </div>
+    </div>
+</div>
+
+
+
+
+<script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
