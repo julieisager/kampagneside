cc34e00a21303ae8bdab9cf66506f6e70d7fa866
Julie <julie@isager.dk>
2023-06-06 11:42:02 +0200

    rettelser

--- includes/menu.php
+++ includes/menu.php
@@ -28,7 +28,6 @@
 
                 <li class="nav-item mx-3 bg-lilla" id="firkant">
                     <a  class="nav-link active" aria-current="page" href="stot.php" id="lillafarve">Støt</a>
-                    <div class="bg-lilla position-absolute top-0 bottom-0 start-0 end-0"></div>
                 </li>
 
 
