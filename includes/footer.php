354d71bb818d66b9a376ff15cb0f36ac4fbf5aa2
Julie <julie@isager.dk>
2023-06-08 00:10:21 +0200

    rettelser

--- includes/footer.php
+++ includes/footer.php
@@ -14,13 +14,13 @@
 
         <div class="col-12 col-md-5 line mb-3 text-center ">
             <h5>Genveje</h5>
-            <p class="">
+
             <div class="text-md-start text-sm-center mx-lg-5"><i class="fa-solid fa-arrow-right mb-2"></i><a href="" class="btn">Søgning</a></div>
             <div class="text-md-start text-sm-center mx-lg-5"><i class="fa-solid fa-arrow-right mb-2"></i><a href="RSV.php" class="btn">Socialøkonomisk Virksomhed</a></div>
             <div class="text-md-start text-sm-center mx-lg-5"><i class="fa-solid fa-arrow-right mb-2"></i><a href="stot.php" class="btn">Støt os</a></div>
             <div class="text-md-start text-sm-center mx-lg-5"><i class="fa-solid fa-arrow-right mb-2"></i><a href="Hvem.php" class="btn">Vores Projekter</a></div>
             <div class="text-md-start text-sm-center mx-lg-5"><i class="fa-solid fa-arrow-right mb-2"></i><a href="https://www.veteranhaven.dk/" class="btn">VeteranHaven.dk</a></div>
-            </p>
+
         </div>
 
         <div class="col-12 col-md-3 mb-3 mt-4 text-center">
@@ -33,24 +33,24 @@
 
 <div class="container-fluid bg-gron p-3">
     <div class="row">
-        <div class="col-5 col-md-6 d-flex align-items-center" id="copyright">
+        <div class="col-4 col-md-6 d-flex align-items-center text-sm-start" id="copyright">
             <p class="card-text"><small class="text-muted">Copyright 2023 VeteranHaven</small></p>
         </div>
-        <div class="col-3 col-md-2 text-end d-flex align-items-center" id="find">
+        <div class="col-3 col-md-2 d-flex align-items-center" id="find">
             Find os her
         </div>
 
-        <div class="col text-end icons d-flex align-items-center">
+        <div class="col  icons d-flex align-items-center">
             <a href="https://www.facebook.com/VeteranHaven" style="color: #4d5154">
                 <i class="fa-brands fa-square-facebook"></i>
             </a>
         </div>
 
-        <div class="col text-end icons d-flex align-items-center">
+        <div class="col icons d-flex align-items-center">
             <i class="fa-brands fa-linkedin"></i>
         </div>
 
-        <div class="col text-end icons d-flex align-items-center">
+        <div class="col icons d-flex align-items-center">
             <i class="fa-brands fa-instagram"></i>
         </div>
 
