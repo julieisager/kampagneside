2306deb5d8600b15ef63457ca913473519acc597
Julie <julie@isager.dk>
2023-06-07 22:23:26 +0200

    rettelser

--- index.php
+++ index.php
@@ -22,20 +22,19 @@
 <?php include "includes/menu.php";?>
 
 
-<div class="container-fluid bg-gron p-3">
+<div class="container-fluid p-3 bg-baggrund" id="halvcirkel2">
 
-
-
-	<div class="row mt-5">
-		<div class="col-12 col-md-5 mb-3 d-flex align-items-center justify-content-center">
-			<div class="card">
-				<div class="card-body">
-					This is some text within a card body.
-                    This is some text within a card body.
-				</div>
+	<div class="row mt-4">
+		<div class="col-12 col-md-5 mb-3 bg-gron d-flex align-items-center justify-content-center">
+			<div class="text-center text-white" id="halvcirkel">
+				<h5>Lorem ipsum dolor sit amet</h5>
+                <br>
+                VeteranHaven ligger i Slagelse kommune. De er et tilbud til folk med besvær i hverdagen, veteraner og alt derimellem.
+                <br><br>
+                Her på siden vil du finde alt nødvendig information omkring haven. Men inden du når så langt, har vi her en video som fortæller meget mere.
 			</div>
 		</div>
-		<div class="col-12 col-md-7">
+		<div class="col-12 col-md-7 bg-baggrund">
 			<div class="ratio ratio-16x9">
                 <iframe width="560" height="315" src="https://www.youtube.com/embed/HeAW3uTwXYg" title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 			</div>
