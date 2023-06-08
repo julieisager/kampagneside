b18769a5c63527e2dbf1c1229138c4c36e9710c3
Julie <julie@isager.dk>
2023-06-08 16:05:42 +0200

    rettelser

--- index.php
+++ index.php
@@ -24,13 +24,13 @@
 
 <?php include "includes/menu.php";?>
 
-<div class="container-fluid bg-baggrund p-3"></div>
+
 
 <div class="container-fluid bg-baggrund" >
 
 	<div class="row">
-		<div class="col-8 p-3 col-md-4 mb-3 bg-gron d-flex align-items-center justify-content-center">
-			<div class=" text-white mt-3 mb-3 mx-4" >
+		<div class=" col-sm-4 p-3 col-md-4 bg-gron d-flex align-items-center justify-content-center">
+			<div class=" text-white mt-3  mx-4" >
 				<h5>Lorem ipsum dolor sit amet</h5>
                 <br>
                 VeteranHaven ligger i Slagelse kommune. De er et tilbud til folk med besvær i hverdagen, veteraner og alt derimellem.
@@ -39,19 +39,22 @@
 
             </div>
 		</div>
-        <div class="col-4 col-md-2 d-flex align-items-center justify-content-center"
+        <div class=" col-sm-2 col-md-2 d-flex align-items-center justify-content-center"
              style="
-            position: relative;
-            width: 100px;
+
+            width: 200px;
             height: auto;
             right: 0;
             border-radius: 0 20rem 20rem 0;
             background-color: rgb(168,227,113);"
         ></div>
-		<div class="col-12 col-md-6 bg-baggrund d-flex align-items-center justify-content-center" style="position: relative">
-			<div class="ratio ratio-16x9">
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/HeAW3uTwXYg" title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-			</div>
+
+		<div class=" col-sm-6 col-md-6 bg-baggrund mt-5 mb-5 d-flex align-items-center justify-content-center" style="position: relative">
+
+                <div class="ratio ratio-16x9" style="margin-left: -100px; top: 10px;">
+                <iframe src="https://www.youtube.com/embed/HeAW3uTwXYg" title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                </div>
+
 		</div>
 	</div>
 </div>
