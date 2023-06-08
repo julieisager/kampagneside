058bd0c191e4fc56d91d25f7089c49b1b9da544e
Julie <julie@isager.dk>
2023-06-08 10:25:37 +0200

    rettelser

--- includes/menu.php
+++ includes/menu.php
@@ -19,9 +19,7 @@
                     <a class="nav-link active" href="Hvem.php" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         Hvem er vi
                     </a>
-                    <button type="button" class="btn btn-secondary" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="bottom" data-bs-content="Bottom popover">
-                        Popover on bottom
-                    </button>
+
                     <ul class="dropdown-menu" style="border: none" aria-labelledby="navbarDropdown">
                         <li id="list-example" class="list-group">
                             <a class="list-group-item list-group-item-action" href="#Veteranhaven">Veteranhaven</a>
